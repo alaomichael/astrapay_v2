0cfb0044e1e5a6544e2b2570cc47dfa09de0acdd
Michael Alao <contactleomax@gmail.com>
2022-04-23 03:08:00 +0100

    fix errors

--- app/Controllers/Http/InvestmentsController.ts
+++ app/Controllers/Http/InvestmentsController.ts
@@ -2,56 +2,53 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Investment from 'App/Models/Investment'
 
 export default class InvestmentsController {
-      public async index({ request}: HttpContextContract)
-        {
-            const investment = await Investment.query().preload('user');
-            return investment
-        }
-        public async show({ request, params}: HttpContextContract)
-        {
-            try {
-                const investment = await Investment.find(params.id);
-                if(investment){
-                    await investment.preload('user')
-                    // await investment.preload('forum');
-                    return investment
-                }
-            } catch (error) {
-                console.log(error)
-            }
+  public async index({ request }: HttpContextContract) {
+    const investment = await Investment.query().preload('user')
+    return investment
+  }
+  public async show({ request, params }: HttpContextContract) {
+    try {
+      const investment = await Investment.find(params.id)
+      if (investment) {
+        await investment.preload('user')
+        // await investment.preload('forum');
+        return investment
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
-        }
+  public async update({ auth, request, params }: HttpContextContract) {
+    const investment = await Investment.find(params.id)
+    if (investment) {
+      investment.title = request.input('title')
+      investment.content = request.input('content')
+      if (await investment.save()) {
+        await investment.preload('user')
+        // await investment.preload('forum')
+        return investment
+      }
+      return // 422
+    }
+    return // 401
+  }
 
-        public async update({ auth, request, params}: HttpContextContract)
-        {
-            const investment = await Investment.find(params.id);
-            if (investment) {
-                investment.title = request.input('title');
-                investment.content = request.input('content');
-                if (await investment.save()) {
-                    await investment.preload('user')
-                    // await investment.preload('forum')
-                    return investment
-                }
-                return; // 422
-            }
-            return; // 401
-        }
-
-        public async store({ auth request, response}: HttpContextContract)
-        {
-            const user = await auth.authenticate();
-            const investment = new Investment();
-            investment.title = request.input('title');
-            investment.content = request.input('content');
-            // investment.forumId = request.input('forum');
-            await user.related('investments').save(investment)
-            return investment
-        }
-        public async destroy({response, auth, request, params}: HttpContextContract)
-        {
-           const user = await auth.authenticate();
-           const investment = await Investment.query().where('user_id', user.id).where('id', params.id).delete();
-           return response.redirect('/dashboard');
-        }
+  public async store({ auth, request, response }: HttpContextContract) {
+    const user = await auth.authenticate()
+    const investment = new Investment()
+    investment.title = request.input('title')
+    investment.content = request.input('content')
+    // investment.forumId = request.input('forum');
+    await user.related('investments').save(investment)
+    return investment
+  }
+  public async destroy({ response, auth, request, params }: HttpContextContract) {
+    const user = await auth.authenticate()
+    const investment = await Investment.query()
+      .where('user_id', user.id)
+      .where('id', params.id)
+      .delete()
+    return response.redirect('/dashboard')
+  }
 }
