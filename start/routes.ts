4b3e2a053cbb7f99bb1d859fe7358109062304c4
Michael Alao <contactleomax@gmail.com>
2022-04-25 12:59:42 +0100

    set up testing

--- start/routes.ts
+++ start/routes.ts
@@ -20,9 +20,9 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-// Route.get('/', async () => {
-//   return { hello: 'world' }
-// })
+Route.get('/', async () => {
+  return { hello: 'world' }
+})
 
 Route.group(() => {
   Route.post('register', 'AuthController.register')
